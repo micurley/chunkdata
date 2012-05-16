ec30b4cc70d59fe7e283f77e032b44ef4aa32827
Aaron McCall <aaron@andyet.net>
2012-05-15 18:31:02 -0700

    bump version to 0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ use_setuptools()
 
 setup(
     name='chunkdata',
-    version='0.0.8',
+    version='0.1.0',
     author='Aaron McCall',
     author_email='aaron@andyet.net',
     packages=find_packages(),
